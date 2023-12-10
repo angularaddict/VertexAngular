d324f98ee5cab370f3a628e16660e971cfb52663
Angular Addict <151340657+angularaddict@users.noreply.github.com>
2023-12-10 22:41:33 +0545

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'customvert':'#6d0002',
+        'lightblue':'#edeff2',
+        'lightgray':'#ECECEC',
+        'custgray':'#515151',
+        'fblue':'#0766ff',
+        'lblue':'#0B65C2',
+        'ipink':'#e200af'
+      },
+    },
+  
+  },
+  plugins: [],
+}
